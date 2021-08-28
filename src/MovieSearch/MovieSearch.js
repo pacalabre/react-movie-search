74e32e7b85921703c864b7cbcabe1114f39126e8
Paul <pacalabre@gmail.com>
2021-08-28 14:35:10 -0700

    Adding MovieSearch component to run a request to the api. Also added this component to the app.js file and storing the results there.

---
+++ src/MovieSearch/MovieSearch.js
@@ -0,0 +1,39 @@
+import "./MovieSearch.css";
+import { useState } from "react";
+
+export function MovieSearch({ movies, setMovies }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  const API_URL = `https://www.omdbapi.com/?s=${searchTerm}&apikey=902755be`;
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const movies = await response.json();
+      setMovies(movies.Search);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <form className="movie-search-form">
+      <label>Search For Movies</label>
+      <input
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+        value={searchTerm}
+        className="movie-search-input"
+        placeholder="Jaws: The Revenge"
+      />
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          getMovies();
+        }}
+        className="movie-search-btn"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
